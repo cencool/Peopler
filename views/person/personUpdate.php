1ddc788118639324c41adb0b9f6cca4509958f9f
cencool <milan.horvath3@gmail.com>
2022-02-01 10:24:46 +0100

    csrf token added to post  for relation delete

--- views/person/personUpdate.php
+++ views/person/personUpdate.php
@@ -37,7 +37,7 @@ if (Yii::$app->session->hasFlash('personAdded')) {
 
 
 <div class='row'>
-	<?php
+    <?php
 
 	$form = ActiveForm::begin([
 		'layout' => 'horizontal',
@@ -49,57 +49,57 @@ if (Yii::$app->session->hasFlash('personAdded')) {
 		],
 	]);
 	?>
-	<div class='col-xs-4'>
-		<p><b><?= Yii::t('app', 'Basic Information') ?></b></p>
-		<hr>
+    <div class='col-xs-4'>
+        <p><b><?= Yii::t('app', 'Basic Information') ?></b></p>
+        <hr>
 
-		<div class='form-group'>
+        <div class='form-group'>
 
-			<?= $form->field($person, 'surname') ?>
-			<?= $form->field($person, 'name') ?>
-			<?= $form->field($person, 'gender') ?>
-			<?= $form->field($person, 'place') ?>
-			<?= Html::activeHiddenInput(
+            <?= $form->field($person, 'surname') ?>
+            <?= $form->field($person, 'name') ?>
+            <?= $form->field($person, 'gender') ?>
+            <?= $form->field($person, 'place') ?>
+            <?= Html::activeHiddenInput(
 				$person,
 				'owner',
 				['value' => Yii::$app->user->id != $person->owner ? $person->owner : Yii::$app->user->id]
 			) ?>
-		</div>
+        </div>
 
 
 
-	</div>
-	<div class='col-xs-8'>
-		<div class='row'>
-			<div class='col-xs-10'>
-				<p><b><?= Yii::t('app', 'Detail Information') ?></b></p>
-				<hr>
-			</div>
-			<div class='col-xs-2'>
-				<div class='form-group'>
-					<?= Html::submitButton(Yii::t('app', 'Update'), ['class' => 'btn  btn-primary pull-right']) ?>
-				</div>
-			</div>
-		</div>
+    </div>
+    <div class='col-xs-8'>
+        <div class='row'>
+            <div class='col-xs-10'>
+                <p><b><?= Yii::t('app', 'Detail Information') ?></b></p>
+                <hr>
+            </div>
+            <div class='col-xs-2'>
+                <div class='form-group'>
+                    <?= Html::submitButton(Yii::t('app', 'Update'), ['class' => 'btn  btn-primary pull-right']) ?>
+                </div>
+            </div>
+        </div>
 
-		<div class='form-group'>
+        <div class='form-group'>
 
-			<?= $form->field($personDetail, 'marital_status') ?>
+            <?= $form->field($personDetail, 'marital_status') ?>
 
-			<?php
+            <?php
 			if ($person->gender == null || $person->gender === 'f') {
 				echo $form->field($personDetail, 'maiden_name');
 			}
 			?>
-			<?= $form->field($personDetail, 'address') ?>
-			<?= $form->field($personDetail, 'note')->textarea(['rows' => 4,]) ?>
+            <?= $form->field($personDetail, 'address') ?>
+            <?= $form->field($personDetail, 'note')->textarea(['rows' => 4,]) ?>
 
-			<?= Html::a(Yii::t('app', 'Attachments') . '(' . $attachmentCount . ')', ['attachment/upload', 'id' => $person->id], ['class' => 'btn btn-primary pull-right']) ?>
-		</div>
+            <?= Html::a(Yii::t('app', 'Attachments') . '(' . $attachmentCount . ')', ['attachment/upload', 'id' => $person->id], ['class' => 'btn btn-primary pull-right']) ?>
+        </div>
 
 
-		<?php ActiveForm::end() ?>
-	</div>
+        <?php ActiveForm::end() ?>
+    </div>
 </div>
 
 <?php
@@ -113,15 +113,15 @@ if (Yii::$app->session->hasFlash('relationDeleted')) {
 $addRelationClass = ($person->id != null) ? 'btn btn-primary pull-left' : 'disabled btn btn-primary pull-left';
 ?>
 <div class='row'>
-	<div class='col-xs-2'>
-		<h4><b><?= Yii::t('app', 'Relations') ?></b></h3>
-	</div>
-	<div class='col-xs-2'>
-		<?= Html::a(Yii::t('app', 'Add Relation'), ['relation/add-relation', 'id' => $person->id], ['class' => $addRelationClass])  ?>
-	</div>
+    <div class='col-xs-2'>
+        <h4><b><?= Yii::t('app', 'Relations') ?></b></h3>
+    </div>
+    <div class='col-xs-2'>
+        <?= Html::a(Yii::t('app', 'Add Relation'), ['relation/add-relation', 'id' => $person->id], ['class' => $addRelationClass])  ?>
+    </div>
 </div>
 <div class='row'>
-	<?php
+    <?php
 	echo GridView::widget([
 		'dataProvider' => $provider,
 		'filterModel' => $searchModel,
@@ -160,7 +160,25 @@ $addRelationClass = ($person->id != null) ? 'btn btn-primary pull-left' : 'disab
 							]
 						);
 					}
-				}
+				},
+				'buttons' => [
+					'delete' => function ($url, $model, $key) {
+						return Html::a(
+							'<svg aria-hidden="true" style="display:inline-block;font-size:inherit;height:1em;overflow:visible;vertical-align:-.125em;width:.875em" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M32 464a48 48 0 0048 48h288a48 48 0 0048-48V128H32zm272-256a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zm-96 0a16 16 0 0132 0v224a16 16 0 01-32 0zM432 32H312l-9-19a24 24 0 00-22-13H167a24 24 0 00-22 13l-9 19H16A16 16 0 000 48v32a16 16 0 0016 16h416a16 16 0 0016-16V48a16 16 0 00-16-16z"/></svg>',
+							$url,
+							[
+								'data' => [
+									'confirm' => Yii::t('app', 'Really delete the record ?'),
+									'method' => 'post',
+									'params' => [
+										'_csrf' => Yii::$app->request->getCsrfToken()
+									]
+								]
+							]
+						);
+					}
+
+				]
 			],
 			[
 				'attribute' => 'relation',
