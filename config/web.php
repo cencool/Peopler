5ca01c3a9019ff26b4f5ca3ad073a637c7e5ea06
cencool <cencul@protonmail.com>
2022-09-10 18:12:09 +0200

    formal changes

--- config/web.php
+++ config/web.php
@@ -4,7 +4,7 @@ $params = require __DIR__ . '/params.php';
 $db = require __DIR__ . '/db.php';
 
 $config = [
-	'id' => 'ydba',
+	'id' => 'peopler',
 	'basePath' => dirname(__DIR__),
 	'bootstrap' => ['log'],
 	'aliases' => [
